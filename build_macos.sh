272edae95e99dd3be0d4ca2c8fb465ee1348e7a9
txthinking <cloud@txthinking.com>
2023-10-28 18:24:35 +0800

    try action

---
+++ build_macos.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+curl https://bash.ooo/nami.sh | bash
+export PATH=$HOME/.nami/bin:$PATH
+
+nami install bun
+brew install llvm@16
+export PATH="$PATH:$(brew --prefix llvm@16)/bin"
+export LDFLAGS="$LDFLAGS -L$(brew --prefix llvm@16)/lib"
+export CPPFLAGS="$CPPFLAGS -I$(brew --prefix llvm@16)/include"
+brew install automake ccache cmake coreutils esbuild gnu-sed go libiconv libtool ninja pkg-config rust
+bun install
+nami install zigup
+zigup 0.12.0-dev.899+027aabf49
+make setup
+make release-bindings -j12
+make release
